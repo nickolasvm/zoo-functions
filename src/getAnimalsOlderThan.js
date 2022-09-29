03d451070a31e8e471fbab7c6e21fb0e5cbd2593
nickolasvm <nickolas64vm@gmail.com>
2022-09-29 17:43:59 -0300

    Resolução requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,21 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
+  const animalsAges = species.filter((specie) =>
+    (specie.name === animal)).map((array) =>
+    array.residents).flat().map((array) =>
+    array.age);
+
+  const smallerAge = animalsAges.find((animalAge) => (animalAge < age));
+  if (smallerAge < age) {
+    return false;
+  }
+  return true;
 }
+
+console.log(getAnimalsOlderThan('otters', 7)); // return false
 
 module.exports = getAnimalsOlderThan;
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,14 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
+const { species } = data;
+
+function getSpeciesByIds(...ids) {
   // seu código aqui
+  if (ids.length === 0) return ids;
+  // return ids;
 }
+
+// console.log(getSpeciesByIds(data.species));
+console.log(getSpeciesByIds('e8481c1d-42ea-4610-8e11-1752cfc05a46'));
 
 module.exports = getSpeciesByIds;
