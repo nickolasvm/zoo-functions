1c223e63e9204dfc6bd09a43b38c216bc80918ac
nickolasvm <nickolas64vm@gmail.com>
2022-09-30 17:42:31 -0300

    Requisito 5

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,28 @@
 const data = require('../data/zoo_data');
 
+const { employees } = data;
+
+const managersId = employees.map((employee) => employee.managers).flat();
+
 function isManager(id) {
   // seu código aqui
+  const findManager = managersId.find((managerId) => id === managerId);
+  return typeof findManager !== 'undefined';
 }
 
 function getRelatedEmployees(managerId) {
   // seu código aqui
+  if (!isManager(managerId)) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+
+  const relatedEmployees = employees.filter((employee) =>
+    employee.managers.find((manager) => manager === managerId));
+
+  const relatedEmployessNames = relatedEmployees.map((employee) =>
+    `${employee.firstName} ${employee.lastName}`);
+
+  return relatedEmployessNames;
 }
 
 module.exports = { isManager, getRelatedEmployees };
