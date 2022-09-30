0ca65898732ee443673b936c9d890e8fa961c133
nickolasvm <nickolas64vm@gmail.com>
2022-09-30 09:40:36 -0300

    Requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,15 @@
 const data = require('../data/zoo_data');
 
+const { employees } = data;
+
 function getEmployeeByName(employeeName) {
   // seu código aqui
+  if (typeof employeeName === 'undefined') return {};
+
+  return employees.find((employee) => {
+    const { firstName, lastName } = employee;
+    return (firstName === employeeName || lastName === employeeName);
+  });
 }
 
 module.exports = getEmployeeByName;
