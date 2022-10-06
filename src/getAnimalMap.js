881efbfa34f91241cca9ad1782a3df032c050198
nickolasvm <nickolas64vm@gmail.com>
2022-10-06 16:39:44 -0300

    Requisito 14

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,93 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
+// recebe { includeNames: true, sex: 'female', sorted: true  }
+// const expected = {
+//   NE: [
+//     { lions: ['Dee', 'Zena'] },
+//     { giraffes: ['Gracia', 'Vicky'] },
+//   ],
+//   NW: [
+//     { tigers: ['Esther', 'Shu'] },
+//     { bears: [] },
+//     { elephants: ['Bea', 'Ilana'] },
+//   ],
+//   SE: [
+//     { penguins: ['Keri'] },
+//     { otters: ['Margherita', 'Mercedes'] },
+//   ],
+//   SW: [
+//     { frogs: ['Annice', 'Cathey'] },
+//     { snakes: ['Paulette'] },
+//   ],
+// };
+
+function filterAnimals(location) {
+  const filteredAnimals = species.filter((specie) =>
+    specie.location === location);
+
+  return filteredAnimals.reduce((arr, animal) =>
+    [...arr, animal.name], []);
+}
+
+function filterAnimalsAndResidents(location, sex, sorted) {
+  const filteredAnimals = species.filter((specie) =>
+    specie.location === location);
+
+  return filteredAnimals.reduce((animalArr, animal) => {
+    const { name, residents } = animal;
+    let animalNames;
+
+    if (sex === 'male' || sex === 'female') {
+      const filteredSex = residents.filter((resident) => resident.sex === sex);
+      animalNames = filteredSex.reduce((resArr, resident) => [...resArr, resident.name], []);
+    } else {
+      animalNames = residents.reduce((resArr, resident) => [...resArr, resident.name], []);
+    }
+
+    if (sorted) return [...animalArr, { [name]: animalNames.sort() }];
+
+    return [...animalArr, { [name]: animalNames }];
+  }, []);
+}
+
+function getAnimalsLocations(bool, sex, sorted) {
+  if (bool) {
+    return species.reduce((obj, specie) => {
+      const { location } = specie;
+
+      return { ...obj, [location]: filterAnimalsAndResidents(location, sex, sorted) };
+    }, {});
+  }
+
+  return species.reduce((obj, specie) => {
+    const { location } = specie;
+
+    return { ...obj, [location]: filterAnimals(location) };
+  }, {});
+}
+
+function checkParameter(param) {
+  switch (typeof param) {
+  case 'object':
+    return true;
+  default:
+    return false;
+  }
+}
+
 function getAnimalMap(options) {
   // seu código aqui
+  if (!checkParameter(options) || !('includeNames' in options) || options.includeNames === false) {
+    return getAnimalsLocations(false);
+  }
+
+  const { sex, sorted } = options;
+  return getAnimalsLocations(true, sex, sorted);
 }
+
+// console.log(filterAnimalsAndResidents('NE'));
+console.table(getAnimalMap({ includeNames: true, sex: 'female', sorted: true }));
 
 module.exports = getAnimalMap;
--- test/getOpeningHours.test.js
+++ test/getOpeningHours.test.js
@@ -1,3 +1,24 @@
 const getOpeningHours = require('../src/getOpeningHours');
 
-describe('Testes da função getOpeningHours', () => {});
+describe('Testes da função getOpeningHours, que recebe 2 argumentos, dia da semana (em inglês) e horas (XX:XX-AM/PM)', () => {
+  it('Quando a função não recebe parâmetros, retorna objeto de dias e horários', () => {
+    const actual = getOpeningHours();
+    const expected = {
+      Tuesday: { open: 8, close: 6 },
+      Wednesday: { open: 8, close: 6 },
+      Thursday: { open: 10, close: 8 },
+      Friday: { open: 10, close: 8 },
+      Saturday: { open: 8, close: 10 },
+      Sunday: { open: 8, close: 8 },
+      Monday: { open: 0, close: 0 },
+    };
+
+    expect(actual).toEqual(expected);
+  });
+
+  it('Quando a função recebe o argumento dia da semana, mas não é dia ou não é string, retorna TypeError', () => {
+    const actual = getOpeningHours(2);
+
+    expect(actual).toThrowError();
+  });
+});
